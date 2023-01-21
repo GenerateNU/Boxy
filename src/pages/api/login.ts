c5e95949e5aad61c05b8fb662a9c1b9ddaa6f5e2
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2023-01-21 04:25:12 -0500

    clean up code for user registration and login

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -11,20 +11,20 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Message>
 ) {
-    if (req.method !== 'POST') {
-        return res.status(405).send({message: 'Only POST request allowed'})
+    if (req.method === 'POST') {
+        const { body } = req;
+
+        // checking for required fields
+        if ("username" in body && "password" in body) {
+            // to implement later: actually check if user exists in database
+
+            const token  = jwt.sign(body['username'], 'secret_key_change_later');
+
+            return res.status(200).send({message: 'logged in', token: token});
+        }
+
+        return res.status(403).json({message: "username and password required"});
     }
 
-    const { body } = req;
-
-    // checking for required fields
-    if ("username" in body && "password" in body) {
-        // to implement later: actually check if user exists in database
-
-        const token  = jwt.sign(body['username'], 'secret_key_change_later');
-
-        return res.status(200).send({message: 'logged in', token: token});
-    }
-
-    return res.status(403).json({message: "username and password required"});
+    return res.status(405).send({message: 'Only POST request allowed'})
 }
--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -6,22 +6,22 @@ type Message = {
     message: string
 }
 
-// POST - register new user
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Message>
 ) {
-    if (req.method !== 'POST') {
-        return res.status(405).send({message: 'Only POST request allowed'})
-    }
+    // POST - register new user
+    if (req.method === 'POST') {
+        const { body } = req;
 
-    const { body } = req;
+        // checking for required fields
+        if ("name" in body && "phone number" in body && "email" in body && "drivers_license_photo" in body && "username" in body && "password" in body) {
+            myDatabase.addUser(body["name"], body["phone number"], body["email"], body["drivers_license_photo"], body["username"], body["password"]);        
+            return res.status(200).send({message: 'user added'});
+        }
 
-    // checking for required fields
-    if ("name" in body && "phone number" in body && "email" in body && "drivers_license_photo" in body && "username" in body && "password" in body) {
-        myDatabase.addUser(body["name"], body["phone number"], body["email"], body["drivers_license_photo"], body["username"], body["password"]);        
-        return res.status(200).send({message: 'user added'});
-    }
+        return res.status(403).json({message: "name, phone number, email, username, and password required"});
+    } 
 
-    return res.status(403).json({message: "name, phone number, email, username, and password required"});
+    return res.status(405).send({message: 'Only POST request allowed'})
 }
