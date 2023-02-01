4dec828f743315cc4bee6a13b31cbe6da8a25e23
tsai-me <tsai.me@northeastern.edu>
2023-01-31 20:53:21 -0500

    Refactor Create Listings

--- src/pages/api/listings/index.ts
+++ src/pages/api/listings/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from 'lib/db';
+import Listings from '@/models/listings';
 
 type Listing = {
     id: string,
@@ -25,43 +26,19 @@ export default async function handler(
   if (req.method === 'POST') {
     try {
       //extract listing info from request body
-      const {name, dates_available, price, description, amenities, space_type, address, city, state, zip_code, space_available} = req.body
-
-      //confirm all fields are entered
-      if (!name || !dates_available || !price || !description || !amenities || !space_type || !address || !city || !state || !zip_code || !space_available) {
-        return res.status(400).json({ message: 'All fields are required' })
-      }
-
+      const { body } = req.body
 
       // Check if the user is logged in
       // (To be added in the future)
 
 
       // Create a new listing using Prisma
-      const newListing = await prisma.listings.create({
-        data: {
-          name: name,
-          dates_available: dates_available,
-          price: price,
-          description: description,
-          amenities: amenities,
-          space_type: space_type,
-          address: address,
-          city: city,
-          state: state,
-          zip_code: zip_code,
-          space_available: space_available,
-          editable: false,
-          created_on: new Date()
-        },
-      })
+      const newListing = new Listings(prisma.listings);
+      await newListing.create(body);
       return res.status(201).json({message: 'Successful'})
     }
     catch (error){
-      return res.status(500).json({ message: String(error) })
-    }
-    finally {
-      await prisma.$disconnect()
+      return res.status(400).json({ message: String(error) })
     }
   }  
 
