9428c797604674d6ebc898783b8534b6a12fc90b
Mario Gonzalez <MGonzalez@StateStreet.com>
2023-02-04 11:22:25 -0500

    refactored login and sign up with model class

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,10 +1,11 @@
-import { PrismaClient, users } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import prisma from "lib/db";
 
 export default class Users {
   constructor(private readonly usersDB: PrismaClient["users"]) {}
 
-  async signUp(data: any) {
+  public async signUp(data: any) {
     try {
       // setting required attributes
       this.setDefaultAttributes(data);
@@ -12,9 +13,39 @@ export default class Users {
       // input validation (can add more validation methods and call them here)
       this.validateInputData(data);
 
+      // create user
+
+      // TODO: We need to encrypt the password before putting it into the database.
       await this.usersDB.create({ data });
-    } catch (e) {
-      throw e;
+      console.log("no issues created user in db");
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == "P2002") {
+          throw new Error("Unique Constraint Violation Failed.");
+        }
+
+        throw new Error(error.message);
+      }
+    }
+  }
+
+  public async logIn(usernameInput: string, passwordInput: string) {
+    try {
+      const user = await prisma.users.findUniqueOrThrow({
+        where: {
+          username: usernameInput,
+        },
+      });
+
+      // check that password match
+      const passwordUser = user["password"];
+      if (passwordInput === passwordUser) {
+        return;
+      } else {
+        throw new Error("Incorrect username or password");
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,30 +1,31 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import jwt from 'jsonwebtoken';
+import type { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import persistentUserInstance from "lib/userInstance";
 
 type Message = {
-    message: string,
-    token?: string
-}
+  message: string;
+  token?: string;
+};
 
 // POST - login
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Message>
 ) {
-    if (req.method === 'POST') {
-        const { body } = req;
+  if (req.method === "POST") {
+    const { body } = req;
+    const token = jwt.sign(body["username"], "secret_key_change_later");
 
-        // checking for required fields
-        if ("username" in body && "password" in body) {
-            // to implement later: actually check if user exists in database
-
-            const token  = jwt.sign(body['username'], 'secret_key_change_later');
-
-            return res.status(200).send({message: 'logged in', token: token});
-        }
-
-        return res.status(403).json({message: "username and password required"});
+    try {
+      await persistentUserInstance.logIn(body["username"], body["password"]);
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(403).send({ message: error.message });
+      }
     }
 
-    return res.status(405).send({message: 'Only POST request allowed'})
+    return res.status(200).send({ message: "logged in", token: token });
+  }
+
+  return res.status(405).send({ message: "Only POST request allowed" });
 }
