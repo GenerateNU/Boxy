fcdfa4fa4ca5418a674215376e2c00c1106673b4
Sunny <76804876+sunnyh2024@users.noreply.github.com>
2023-02-13 16:43:54 -0500

    small bug fix in [id].ts

--- src/pages/api/listings/[id].ts
+++ src/pages/api/listings/[id].ts
@@ -11,7 +11,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<listings | null | Message>
 ) {
-  if ("id" ! in req.query) {
+  if (!("id" in req.query)) {
     return res.status(400).json({message: "missing id query in parameter string"})
   }
 
