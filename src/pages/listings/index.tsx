550de0c7a1f5622e9bdd8bc948029fd97cd44238
alderwhiteford <whiteford.a@northeastern.edu>
2023-02-23 19:22:13 -0500

    Added listing page, colors to tailwind, and icons

--- src/pages/listings/index.tsx
+++ src/pages/listings/index.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useState } from "react";
+import Listing from "./listing";
 
 type Listing = {
   listing_id: string;
@@ -7,17 +9,30 @@ type Listing = {
   proximity: string;
 };
 
-export default function ListingsPage({ listings }) {
+export default function ListingsPage({ listings }: any) {
+  const [tabState, setTabState] = useState("all")
+
+  const listing_tab = (state: string, text: string) => {
+    return (
+    <button onClick={() => setTabState(state)} className={tabState == state ? "flex justify-center border-b-2 border-b-black" : "flex justify-center border-b-2"}>{text}</button>
+  )}
+  
   return (
-    <div>
-      {listings.map((listing: Listing) => {
-        return (
-          <h1 key={listing.listing_id}>
-            {"$" + listing.price + " " + listing.name + " " + listing.proximity}
-          </h1>
-        );
-      })}
-      <Link href="/login">Login</Link>
+    <div className="container flex justify-center min-w-full pt-20">
+      <div className="w-[50vw] flex-col">
+        <h1 className="text-3xl pb-10">My Listings</h1>
+        <div className="grid grid-cols-4 w-4/6 h-[7vh] mb-5">
+          {listing_tab('all', 'All')}
+          {listing_tab('listed', 'Listed')}
+          {listing_tab('unlisted', 'Unlisted')}
+          {listing_tab('progress', 'In Progress')}
+        </div>
+        {listings.map((listing: Listing) => {
+          return (
+            <Listing listing={listing}/>
+          );
+        })}
+      </div>
     </div>
   );
 }
