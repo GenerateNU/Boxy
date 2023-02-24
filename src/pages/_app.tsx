9650b6f0848e03fe39219d71f02ae68201d52c05
alderwhiteford <whiteford.a@northeastern.edu>
2023-02-23 19:45:01 -0500

    Fixed app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { useSession, signIn, signOut } from "next-auth/react"
 
