f9936fecbdf9b3a16fd4f722ace6ff6641637642
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-02-25 15:09:53 -0500

    Add a navBar component to the components folder and then implemented it into the _app.tsx file to load for every page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import NavBar from "src/components/NavBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <NavBar />
       <Component {...pageProps} />
       <button onClick={() => signIn()}>click here to login</button>
       <button onClick={() => signOut()}>click here to logout</button>
