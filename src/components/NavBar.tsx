0b8c5205396238a0fb47d682b7cb2c4b9a46b649
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2023-02-25 22:59:26 -0500

    link pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,53 +1,64 @@
+import { sign } from "crypto";
+import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
 import { useState } from "react";
-import {BiUserCircle} from 'react-icons/bi'
-
+import { BiUserCircle } from "react-icons/bi";
 
 export default function NavBar() {
+  const nav_bar_button = (state: string, text: any) => {
+    const [navState, setNavState] = useState("");
+    const { data, status } = useSession();
 
-    const nav_bar_button = (state: string, text: any) => {
-      const [navState, setNavState] = useState("");
-    
-      const handleClick = (state: string) => {
-        setNavState(state);
-        if(state=="browse")
-                   window.location.replace("http://localhost:3000/browse");
-               else if(state=="reservations")
-                   window.location.replace("http://localhost:3000/reservations");
-               else if(state=="myListings")
-                   window.location.replace("http://localhost:3000/listings");
-            else if(state=="userIcon")
-            window.location.replace("http://localhost:3000/account");
-      };
-    
-      return (
-        <button
-          onClick={() => handleClick(state)} 
-          className={
-            navState == state
-              ? "flex justify-center text-black"
-              : "flex justify-center hover:text-black"
-          }
-        >
-          {text}
-        </button>
-      );
+    const handleClick = (state: string) => {
+      setNavState(state);
+      if (state == "browse")
+        // window.location.replace("http://localhost:3000/browse");
+        window.location.replace("http://localhost:3000/search/results");
+      else if (state == "reservations")
+        window.location.replace("http://localhost:3000/reservations");
+      else if (state == "myListings")
+        window.location.replace("http://localhost:3000/search/results");
+      else if (state == "userIcon")
+        if (status === "unauthenticated") {
+          signIn();
+        } else {
+          // window.location.replace("http://localhost:3000/account");
+          alert("signed in");
+        }
     };
-  
+
     return (
-      <div>
-        <nav className="relative container min-w-full h-16 border-b-2 border-gray">
-          <div className="flex justify-between items-center container mx-auto h-full ">
-            <div className="text-center font-Inter font-semibold text-xl text-[#097275]">
-              BOXY
-            </div>
-            <div className="flex items-center gap-10 text-[#C4C4C4]">
-              {nav_bar_button("browse", "Browse")}
-              {nav_bar_button("reservations", "Reservations")}
-              {nav_bar_button("myListings", "My Listings")}
-              {nav_bar_button("userIcon", <BiUserCircle size={30}/>)}
-            </div>
-          </div>
-        </nav>
-      </div>
+      <button
+        onClick={() => handleClick(state)}
+        className={
+          navState == state
+            ? "flex justify-center text-black"
+            : "flex justify-center hover:text-black"
+        }
+      >
+        {text}
+      </button>
     );
-  }
+  };
+
+  return (
+    <div>
+      <nav className="relative container min-w-full h-16 border-b-2 border-gray">
+        <div className="flex justify-between items-center container mx-auto h-full ">
+          <Link
+            href={"http://localhost:3000/"}
+            className="text-center font-Inter font-semibold text-xl text-[#097275]"
+          >
+            BOXY
+          </Link>
+          <div className="flex items-center gap-10 text-[#C4C4C4]">
+            {nav_bar_button("browse", "Browse")}
+            {nav_bar_button("reservations", "Reservations")}
+            {nav_bar_button("myListings", "My Listings")}
+            {nav_bar_button("userIcon", <BiUserCircle size={30} />)}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
