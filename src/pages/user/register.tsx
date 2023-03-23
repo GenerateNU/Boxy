2c8bb4bde575825696d5fedd7ea8c87326e8ce24
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2023-03-22 23:00:17 -0400

    add user registration page

---
+++ src/pages/user/register.tsx
@@ -0,0 +1,8 @@
+export default function UserRegistrationPage() {
+    return (
+      <div className="container flex justify-center min-w-full pt-16">
+        <h1>registration page</h1>
+      </div>
+    );
+  }
+  
