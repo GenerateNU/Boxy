683a5ca62253bb4087a09c63ec866a23b352faa6
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2023-03-22 23:40:23 -0400

    update rservation page

--- src/pages/reservations/mine.tsx
+++ src/pages/reservations/mine.tsx
@@ -7,6 +7,19 @@ export default function ReservationsPage() {
         click here to go to a dummy reservation's detail page (maybe we can make
         this just a modal)
       </button>
+      <h1>reservations requested (stasher)</h1>
+      <h1>reservations accepted (stasher)</h1>
+      <h1>upcoming reservations (stasher)</h1>
+      <h1>current reservations (stasher)</h1>
+      <h1>past reservations (stasher)</h1>
+
+      <h1>reservations requested(host)</h1>
+      <h1>reservations accepted (host)</h1>
+      <h1>upcoming reservations (host)</h1>
+      <h1>current reservations (host)</h1>
+      <h1>past reservations (host)</h1>
+
+      <h1>add toggle between stasher and host view</h1>
     </div>
   );
 }
