19cff4361e5095bc858034a3e108d79a8c9bc663
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2023-03-22 22:48:28 -0400

    add listing reservation page

--- src/pages/listings/[listingID]/reserve.tsx
+++ src/pages/listings/[listingID]/reserve.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 
-export default function ListingDetailsPage() {
+export default function ListingReservationPage() {
   const router = useRouter();
   const { listingID } = router.query;
 
   return (
     <div className="flex flex-col pt-16">
-      <h1>listing {listingID} details go here</h1>
+      <h1>reservation for listing {listingID} page go here</h1>
     </div>
   );
 }
