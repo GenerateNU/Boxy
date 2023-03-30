6f9517fe8c14e0089a5aea4ea7cb1e684206fbb3
Alison Ryan <alisonryan@Alisons-MacBook-Air.local>
2023-03-29 21:51:34 -0400

    delete reservations

--- src/pages/api/reservations/index.ts
+++ src/pages/api/reservations/index.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { reservations } from "@prisma/client";
+import { getServerSession, Session } from "next-auth";
 import persistentReservationInstance from "lib/reservationInstance";
+import Utils from "@/utils";
 
 type Message = {
   message: string;
@@ -14,6 +16,7 @@ export default async function handler(
   const supportedRequestMethods: { [key: string]: Function } = {
     GET: getReservationsGivenFilters,
     POST: createReservation,
+    DELETE: cancelReservation,
   };
 
   if (req.method) {
@@ -41,4 +44,38 @@ async function createReservation(
   }
 
   return res.status(200).send({ message: "created reservation" });
+}
+
+async function cancelReservation(
+  req: NextApiRequest,
+  res: NextApiResponse<Message>,
+  session: Session
+) {
+  if (!session) {
+    return res.status(401).send({ message: "user is not authenticated." });
+  }
+
+  try {
+    const now = new Date();
+    const email = session.user?.email
+    const userID = await Utils.getUserId(email); 
+    const reservation = await persistentReservationInstance.getReservation(req.body.id)
+    if(!reservation) {
+      return res.status(401).send({message : "reservation does not exist"})
+    }
+    for(var date of reservation.dates_requested) {
+      if(date.getTime() > now.getTime()) {
+        return res.send({message: "cannot cancel after dates requested"})
+      }
+    }
+    if(userID != reservation.host_id && userID != reservation.stasher_id) {
+      return res.status(401).send({ message: "only host or stasher can cancel this reservation"})
+    }
+    
+    await persistentReservationInstance.cancelReservation(req.body.id)
+
+  } catch(e) {
+    throw(e);
+  }
+
 }
